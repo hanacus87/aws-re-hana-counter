7d1423155808a52b28bb006e6e3f15f8b71710d3
hanacus87 <hanacus87@gmail.com>
2026-06-23 13:15:27 +0900

    build: disable Vite source maps

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,5 @@ export default defineConfig({
       exclude: [/^\/(?!api\/|auth\/).*/],
     }),
   ],
-  build: { outDir: "dist/client" },
+  build: { outDir: "dist/client", sourcemap: false },
 });
